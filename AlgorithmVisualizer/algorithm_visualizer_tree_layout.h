a99f839be6fc436f210a61aa3330767e353bed15
GeorgeUF <itismymailnow@gmail.com>
2019-03-22 16:34:00 +0300

    AlgorithmVisualizerTreeLayout: draws node connection, refactoring of MergedTree

--- AlgorithmVisualizer/algorithm_visualizer_tree_layout.h
+++ AlgorithmVisualizer/algorithm_visualizer_tree_layout.h
@@ -8,6 +8,12 @@ class AlgorithmVisualizerTreeLayout: public AlgorithmVisualizerBase{
 public:
     AlgorithmVisualizerTreeLayout(){
         name = "Tree layout";
+        setUpTestTree();
+    }
+    virtual ~AlgorithmVisualizerTreeLayout() override{
+        for(auto node:nodes){
+            delete node;
+        }
     }
 public slots:
     virtual void reset() override{
@@ -19,23 +25,96 @@ public slots:
 protected:
     virtual void paintEvent(QPaintEvent *event) override{
         QPainter painter(this);
-        auto nodesPos =treeLayout.getNodesPositions();
-        if(nodesPos.size()>0){
-            painter.setBrush(Qt::cyan);
-            for(int i=0;i<nodesPos.size();i++){
-                for(int j=0;j<nodesPos.at(i).size();j++){
-                    auto currLevel = i;
-                    auto currX = nodesPos.at(i).at(j);
-                    painter.drawRect(currX*nodeXDistance + margin, currLevel*nodeYDistance+margin,nodeSize,nodeSize );
+        painter.setBrush(Qt::cyan);
+        drawNode(painter,root);
+    }
+    void drawNode(QPainter &painter, TreeLayoutNode *node, bool hasParent=false){
+        if(node->isSet){
+            int x = node->position * (nodeSize + xGap) + margin;
+            int y = node->level * (nodeSize + yGap) + margin;
+            painter.drawRect(x, y, nodeSize, nodeSize);
+            if(hasParent){
+                painter.drawLine(x+nodeSize/2,y,x+nodeSize/2,y-yGap/2);
+            }
+            bool hasChildren = node->children.size() > 0;
+            if(hasChildren){
+                painter.drawLine(x+nodeSize/2,y+nodeSize,x+nodeSize/2,y+nodeSize+yGap/2);
+                bool hasMoreThanOneChild = node->children.size() > 1;
+                if(hasMoreThanOneChild){
+                    auto firstChild = node->children.at(0);
+                    auto lastChild = node->children.at(node->children.size()-1);
+                    bool childsAreSet = firstChild->isSet && lastChild->isSet;
+                    if(childsAreSet){
+                        int xFirstChild = firstChild->position * (nodeSize + xGap) + margin;
+                        int xLastChild = lastChild->position * (nodeSize + xGap) + margin;
+                        painter.drawLine(xFirstChild + nodeSize/2,
+                                         y + nodeSize + yGap/2,
+                                         xLastChild + nodeSize/2,
+                                         y + nodeSize + yGap/2);
+                    }
                 }
+            }
+            for(auto child: node->children){
+                drawNode(painter,child, true);
             }
         }
     }
+    void setUpTestTree(){
+        for(int i=0;i<15;i++){
+            nodes.push_back(new TreeLayoutNode());
+        }
+        /*
+         *
+         * lvl0                   n0
+         *                         |
+         *                 +-------+------+
+         *                 |              |
+         * lvl1           n1             n6
+         *                 |              |
+         *           +-----+-----+     +-----+
+         *           |     |     |     |     |
+         * lvl2     n2    n4    n5    n7    n13
+         *           |                 |
+         *           +     +-----+-----+-----+-----+
+         *           |     |     |     |     |     |
+         * lvl3     n3    n8    n9    n10   n11   n12
+         *                                   |
+         *                                   +
+         *                                   |
+         * lvl4                             n14
+         *
+         * xCoords:  0     1     2     3     4     5
+         */
+        nodes.at(2)->children.push_back(nodes.at(3));
+
+        nodes.at(11)->children.push_back(nodes.at(14));
+
+        nodes.at(1)->children.push_back(nodes.at(2));
+        nodes.at(1)->children.push_back(nodes.at(4));
+        nodes.at(1)->children.push_back(nodes.at(5));
+
+        nodes.at(7)->children.push_back(nodes.at(8));
+        nodes.at(7)->children.push_back(nodes.at(9));
+        nodes.at(7)->children.push_back(nodes.at(10));
+        nodes.at(7)->children.push_back(nodes.at(11));
+        nodes.at(7)->children.push_back(nodes.at(12));
+
+        nodes.at(6)->children.push_back(nodes.at(7));
+        nodes.at(6)->children.push_back(nodes.at(13));
+
+        nodes.at(0)->children.push_back(nodes.at(1));
+        nodes.at(0)->children.push_back(nodes.at(6));
+
+        root = nodes.at(0);
+        treeLayout.setTree(root);
+    }
 protected:
     TreeLayout treeLayout;
+    TreeLayoutNode *root;
+    std::vector<TreeLayoutNode*> nodes;
     int nodeSize = 50;
-    int nodeXDistance = 65;
-    int nodeYDistance = 65;
+    int xGap = 15;
+    int yGap = 15;
     int margin = 200;
 };
 
--- Algorithms/TreeLayout/TreeLayout/tree_layout.h
+++ Algorithms/TreeLayout/TreeLayout/tree_layout.h
@@ -12,6 +12,9 @@ friend class test_case_name##_##test_name##_Test
 struct TreeLayoutNode{
     std::vector<TreeLayoutNode*> children;
     double width = 1;
+    bool isSet = false;
+    int level=0;
+    double position=0;
 };
 
 class MergedTree{
@@ -21,24 +24,9 @@ public:
         levels.push_back({0,node->width});
     }
     void add(MergedTree *another, int levelOffset=1){
-        auto totalLevels = std::max(this->levels.size(),another->levels.size()+levelOffset);
-        if(this->levels.size() < totalLevels){
-            this->levels.resize(totalLevels,{0,0});
-            levelsFirstTreeId.resize(totalLevels,0);
-            levelsLastTreeId.resize(totalLevels,0);
-        }
+        resizeContainers(another, levelOffset);
         //Find how much entire another tree will be shifted right
-        double maxAnotherOffset=0;
-        int maxAnotherOffsetLevel=0;
-        for(int i=levelOffset;i<levelOffset+another->levels.size();i++){
-            auto &thisLevel = this->levels.at(i);
-            auto &anotherLevel = another->levels.at(i-levelOffset);
-            double currAnotherOffset = thisLevel.at(1) - anotherLevel.at(0);
-            if(currAnotherOffset > maxAnotherOffset){
-                maxAnotherOffset = currAnotherOffset;
-                maxAnotherOffsetLevel = i;
-            }
-        }
+        double minOffset = computeMinOffset(another, levelOffset);
 
         for(int i=levelOffset;i<levelOffset+another->levels.size();i++){
             auto &thisLevel = this->levels.at(i);
@@ -49,10 +37,10 @@ public:
             //Check if there is no nodes on the level
             if(thisLevel.at(1)==0){
                 //Take xMin (thisLevel.at(0)) from anotherLevel
-                thisLevel.at(0) = anotherLevel.at(0) + maxAnotherOffset;
+                thisLevel.at(0) = anotherLevel.at(0) + minOffset;
                 levelsFirstTreeId.at(i) = treesAdded+1;
             }
-            thisLevel.at(1) =  anotherLevel.at(0) + anotherLevelWidth + maxAnotherOffset;
+            thisLevel.at(1) =  anotherLevel.at(0) + anotherLevelWidth + minOffset;
             levelsLastTreeId.at(i) = treesAdded+1;
         }
         spreadSubtreesEqually();
@@ -64,37 +52,57 @@ public:
         childMergedTrees.push_back(another);
         add(another.get());
     }
-    std::vector<std::vector<double>> getNodesCoords(const double rootOffset, bool applyRootOffset=true){
-        std::vector<std::vector<double>> result;
-
+    void assignValuesToNodes(const int rootLevel, const double rootOffset, bool applyRootOffset=true){
         double xOffset = 0;
         if(applyRootOffset) xOffset = rootOffset - levels.at(0).at(0);
         //Add coords of root node
-        result.push_back({levels.at(0).at(0) + xOffset});
-        //Add coords of lower node based of child merged trees
-        if(levels.size() > 1){
-            auto levelOneWidth = levels.at(1).at(1) - levels.at(1).at(0) - 1;
-            double nodeSpacing = 0;
-            if(childMergedTrees.size()>1){
-                nodeSpacing = levelOneWidth/((double)(childMergedTrees.size()-1));
-            }
+        node->position = levels.at(0).at(0) + xOffset;
+        node->level = rootLevel;
+        node->isSet = true;
+        if(childMergedTrees.size() > 0){
+            double nodeSpacing = getSpacing();
             for(int i=0;i<childMergedTrees.size();i++){
                 auto child = childMergedTrees.at(i);
-                auto currChildXOffset = levels.at(1).at(0) + nodeSpacing * i + xOffset;
-                auto childNodeCoords = child->getNodesCoords(currChildXOffset);
-                //Resize result if needed
-                if(result.size() < childNodeCoords.size()+1){
-                    result.resize(childNodeCoords.size()+1);
-                }
-                //Merge result and curr child result
-                for(int i=1;i<childNodeCoords.size()+1;i++){
-                    result.at(i).insert(result.at(i).end(),childNodeCoords.at(i-1).begin(),childNodeCoords.at(i-1).end());
-                }
+                auto currOffset = levels.at(1).at(0) + nodeSpacing * i + xOffset;
+                child->assignValuesToNodes(rootLevel+1,currOffset);
             }
         }
-        return result;
     }
 protected:
+    //Computes min x offset of another tree
+    //The result will determine how much entire another tree will be shifted right
+    double computeMinOffset(MergedTree *another, int levelOffset=1)
+    {
+        double minAnotherOffset=0;
+        for(int i=levelOffset;i<levelOffset+another->levels.size();i++){
+            auto &thisLevel = this->levels.at(i);
+            auto &anotherLevel = another->levels.at(i-levelOffset);
+            double currAnotherOffset = thisLevel.at(1) - anotherLevel.at(0);
+            if(currAnotherOffset > minAnotherOffset){
+                minAnotherOffset = currAnotherOffset;
+            }
+        }
+        return  minAnotherOffset;
+    }
+    //Resizes used vectors to accomodate data from another
+    void resizeContainers(MergedTree *another, int levelOffset=1)
+    {
+        auto totalLevels = std::max(this->levels.size(),another->levels.size()+levelOffset);
+        if(this->levels.size() < totalLevels){
+            this->levels.resize(totalLevels,{0,0});
+            levelsFirstTreeId.resize(totalLevels,0);
+            levelsLastTreeId.resize(totalLevels,0);
+        }
+    }
+    //Computes distances between level 1 nodes
+    double getSpacing(){
+        auto levelOneWidth = levels.at(1).at(1) - levels.at(1).at(0) - node->width;
+        double nodeSpacing = 0;
+        if(childMergedTrees.size()>1){
+            nodeSpacing = levelOneWidth/((double)(childMergedTrees.size()-1));
+        }
+        return  nodeSpacing;
+    }
     void centerZeroLevel(){
         if(levels.size()>1){
             auto firstLevelCenter = levels.at(1).at(0) +
@@ -145,44 +153,7 @@ protected:
 
 class TreeLayout{
 public:
-    TreeLayout(){
-        TreeLayoutNode tr0;
-
-        auto mt20  = std::make_shared<MergedTree>(&tr0);
-        auto mt21  = std::make_shared<MergedTree>(&tr0);
-        auto mt22  = std::make_shared<MergedTree>(&tr0);
-        auto mt23  = std::make_shared<MergedTree>(&tr0);
-        auto mt24  = std::make_shared<MergedTree>(&tr0);
-        auto mt25  = std::make_shared<MergedTree>(&tr0);
-        auto mt26  = std::make_shared<MergedTree>(&tr0);
-        auto mt27  = std::make_shared<MergedTree>(&tr0);
-        auto mt28  = std::make_shared<MergedTree>(&tr0);
-        auto mt29  = std::make_shared<MergedTree>(&tr0);
-        auto mt30  = std::make_shared<MergedTree>(&tr0);
-        auto mt31  = std::make_shared<MergedTree>(&tr0);
-        auto mt32  = std::make_shared<MergedTree>(&tr0);
-        auto mt33  = std::make_shared<MergedTree>(&tr0);
-
-        mt21->add(mt20);
-
-        mt24->add(mt21);
-        mt24->add(mt22);
-        mt24->add(mt23);
-
-        mt27->add(mt29);
-        mt27->add(mt30);
-        mt27->add(mt31);
-        mt27->add(mt32);
-        mt27->add(mt33);
-
-        mt26->add(mt27);
-        mt26->add(mt28);
-
-        mt25->add(mt24);
-        mt25->add(mt26);
-
-        rootMergedTree = mt25;
-    }
+    TreeLayout(){}
     TreeLayout(TreeLayoutNode *root){
         setTree(root);
     }
@@ -190,10 +161,7 @@ public:
         clear();
         this->root = root;
         rootMergedTree = buildMergedTree(root);
-    }
-    //Return vector of xMin values for each node per level
-    std::vector<std::vector<double>> getNodesPositions(){
-        return rootMergedTree->getNodesCoords(0,false);
+        rootMergedTree->assignValuesToNodes(0,0,false);
     }
     void clear(){
         root = nullptr;
